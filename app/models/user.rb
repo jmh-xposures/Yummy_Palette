39b7ede9cc5298ade310858194539f98db38d9fe
Junior Rivas <jrtimmobilier@gmail.com>
2020-08-26 14:52:36 -0400

    seed done for test

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  belongs_to :cuisine
+  # belongs_to :cuisine
   has_many :follows
 
   validates :first_name, presence: true
