ed39b516a1cfb9a906e4b70ba6e59aa35b9c594b
Juan Miguel Hernandez <jmh@jmhernandez.com>
2020-09-01 13:59:46 -0400

    cleanup again

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
-    raise
     @cuisines = Cuisine.all
     @restaurants = Restaurant.latest_upvoted
     @cuisine_count = Cuisine.count
