89213f0c5e95060dc7204961a5a6b04dad63447f
Juan Miguel Hernandez <jmh@jmhernandez.com>
2020-09-01 22:29:46 -0400

    follow user button created on profile page

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   end
 
   resources :users, only: [:show] do 
-    resources :follows, only: [:create]
+    resources :follows, only: [:create, :destroy]
   end
 
   resources :dishes, only: [:create]
