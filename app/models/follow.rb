3c6348aaaeded3007a2a16e598ea0a4323ffa0ce
Juan Miguel Hernandez <jmh@jmhernandez.com>
2020-09-02 22:50:49 -0400

    testing

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -2,4 +2,5 @@ class Follow < ApplicationRecord
    belongs_to :follower, foreign_key: 'follower_id', class_name: 'User'
    belongs_to :followed, foreign_key: 'followed_id', class_name: 'User'
    has_one_attached :photo
+   
 end
